b72f4eaffd62e607198cc2c84cdc6c57b9bb00cf
Shilcof <robert.shilcof@gmail.com>
2020-10-21 11:41:30 +0100

    commit tree 7548d25fe3d31825e29300ac202f047bb8242710

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each.with_index{|name,i| puts "#{i+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(names)# code an argument here
   # Your code here
+  names.map{|name| "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(names)# code an argument here
   # Your code here
+  names.any?{|name| name.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(names)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  names.detect{|name| cheese_types.include?(name)}
 end
